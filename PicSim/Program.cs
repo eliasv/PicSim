5f6898f86a0317018ded22eaaea72b4df748d69d
Elias V. Beauchamp <elias.beauchamp@upr.edu>
2015-10-27 13:20:35 -0400

    Initial commit

---
+++ PicSim/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using(StreamReader sr = new StreamReader("flash.hex"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        String line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
